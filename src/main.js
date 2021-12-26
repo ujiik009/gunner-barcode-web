1865419aa629418d0025ea389e69a923b3688fd9
som <amondayartyuphom@example.com>
2021-12-26 14:52:55 +0700

    vdo page

--- src/main.js
+++ src/main.js
@@ -7,7 +7,17 @@ import StickerDesign from '@/pages/StickerDesign.vue'
 import Claim from '@/pages/Claim.vue'
 import Vdo from '@/pages/Vdo.vue'
 import * as VueGoogleMaps from 'vue2-google-maps'
+import LazyTube from "vue-lazytube";
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.config.productionTip = false
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue)
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin)
+Vue.use(LazyTube);
 Vue.use(VueGoogleMaps, {
   load: {
     key: 'AIzaSyCW2fzdLg2Cahf1PTLjXSEO8_ek5WYmNP4',
--- src/pages/Vdo.vue
+++ src/pages/Vdo.vue
@@ -1,13 +1,38 @@
 <template>
-  <div>Vdo</div>
+  <div>
+    <div><h3>วีดีโอแนะนำ</h3></div>
+    <div>
+      <b-container class="bv-example-row">
+        <b-row class="justify-content-md-center">
+          <b-col v-for="(vdo,index) in vdos" :key="index" col lg="6" md="6">
+           <LazyYoutube :src="vdo" /> 
+          </b-col>
+        </b-row>
+      </b-container>
+      <!-- Youtube Embed -->
+      <!-- <LazyYoutube src="https://www.youtube.com/watch?v=Ua5WLUzgbNs" /> -->
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      vdos: [
+        "https://www.youtube.com/watch?v=Ua5WLUzgbNs",
+        "https://www.youtube.com/watch?v=n6nAo2QylH8",
+        "https://www.youtube.com/watch?v=C7H_ciLNqw4",
+        "https://www.youtube.com/watch?v=ObkFvwvbIYg",
+        "https://www.youtube.com/watch?v=uOJaPH7HuI4",
+        "https://www.youtube.com/watch?v=rfr2tTlhUPk",
+        "https://www.youtube.com/watch?v=twPfxOVczUs",
+        ],
 
-}
+    };
+  },
+};
 </script>
 
 <style>
-
 </style>
