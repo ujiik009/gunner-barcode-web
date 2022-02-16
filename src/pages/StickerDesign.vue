4aed66c45f29e07fe2095db55187dac3165f4776
som <amondayartyuphom@example.com>
2022-02-16 22:52:43 +0700

    -

--- src/pages/StickerDesign.vue
+++ src/pages/StickerDesign.vue
@@ -1,13 +1,91 @@
 <template>
-  <div>StickerDesign</div>
+  <div>
+    <div class="box-content">
+      <div class="box-item">
+        <div class="item">
+          <div>เลือกชนิดสติ๊กเกอร์</div>
+          <div>
+            <select class="select-item" name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <div class="item">
+          <div>เลือกขนาด</div>
+          <div>
+            <select class="select-item" name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <div class="item">
+          <div>เลือกรูปแบบ ลักษณะ</div>
+          <div>
+            <select class="select-item" name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+      </div>
+      <div class="box-item">
+        <div class="item">
+          <div>เลือกสี</div>
+          <div>
+            <select class="select-item" name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+        <div class="item">
+          <div>เลือกจำนวนการผลิต</div>
+          <div>
+            <select class="select-item" name="" id="">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-
+.box-content {
+  display: flex;
+  padding: 10px;
+}
+.box-item {
+  flex: 1;
+}
+.select-item{
+  width: 250px;
+}
+.item{
+  margin: 20px;
+  font-size: 22px;
+}
 </style>
